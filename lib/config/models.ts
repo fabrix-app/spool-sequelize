1498e86c75c80eab57bf160f43bc2d02b7006dc1
scott-wyatt <scottwyatt86@gmail.com>
2018-08-20 19:32:03 -0400

    [chore] update docs

--- lib/config/models.ts
+++ lib/config/models.ts
@@ -2,7 +2,7 @@
  * Models Configuration
  * (app.config.models)
  *
- * @see {@link https://fabrix.app/doc/config/models}
+ * @see {@link https://fabrix.app/docs/config/models}
  */
 export const models = {
   // Leave the file even empty because if the file is not present, env config is not merged
