ba1afd6e6e295e7a370f01422eb9d4d615ae556a
scott-wyatt <scottwyatt86@gmail.com>
2018-11-24 16:07:18 -0500

    [feat] coverage and release

---
+++ test/unit/lib/errors.test.js
@@ -0,0 +1,28 @@
+'use strict'
+const assert = require('assert')
+const errors = require('../../../dist/errors')
+
+describe('errors', () => {
+  describe('#ModelErrors', () => {
+    it('should create E_NOT_FOUND Error', () => {
+      const err = new errors.ModelError('E_NOT_FOUND', 'Not Found')
+      assert(err)
+      assert.equal(err.statusCode, 404)
+    })
+    it('should create E_FORBIDDEN Error', () => {
+      const err = new errors.ModelError('E_FORBIDDEN', 'Forbidden')
+      assert(err)
+      assert.equal(err.statusCode, 403)
+    })
+    it('should create E_BAD_REQUEST Error', () => {
+      const err = new errors.ModelError('E_BAD_REQUEST', 'Bad Request')
+      assert(err)
+      assert.equal(err.statusCode, 400)
+    })
+    it('should create E_UNKNOWN Error', () => {
+      const err = new errors.ModelError('E_UNKNOWN', 'Unknown Error Request')
+      assert(err)
+      assert.equal(err.statusCode, 500)
+    })
+  })
+})
