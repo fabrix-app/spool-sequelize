8840b7c54e84c4a8f83b309349be0db8db47027b
scott-wyatt <scottwyatt86@gmail.com>
2019-05-01 11:16:57 -0500

    [feat] attach reference of the model to the instance

--- test/integrations/spool.test.js
+++ test/integrations/spool.test.js
@@ -67,6 +67,13 @@ describe('Spool', () => {
       })
   })
 
+  it('instance should have access to model', (done) => {
+    const instance = global.app.models.testModel.build({name: 'test'})
+    assert.ok(instance.model)
+    assert.equal(instance.model, global.app.models.testModel)
+    done()
+  })
+
   it('should build and save an instance with associations', (done) => {
     const instance = global.app.models.User.build({
       name: 'test',
